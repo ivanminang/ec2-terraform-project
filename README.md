a72559b45a2c2cf263e7b4c303787c2836b1cc3f
Emile Ivan MINANG <ivanminang@gmail.com>
2023-04-27 19:34:37 -0400

    provisioning ec2 on aws with terraform

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ec2-terraform-project
 provisioning ec2 on aws using terraform
+# How to use the configuration file
+Clone it to your local computer
+configure your aws credentials for the user 
+cd into the cloned repository
+press the following terraformcommands:
+fmt, init, validate, plan and apply
+# Provider
+The provider here is aws provider
+# Region
+This Vpc is created in us-west-2 
---
+++ main.tf
@@ -0,0 +1,22 @@
+# Build an EC2 using Terraform on aws
+# Terraform Block 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+# Configure the aws Provider
+provider "aws" {
+  region     = "us-west-2"
+}
+# create an ec2
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-009c5f630e96948cb" # us-west-2
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-ec2"
+  }
+}
